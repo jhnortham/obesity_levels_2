3dba396b5addcfdd9aaf3e5a294865b3670f059a
Jamie Heather Northam <heathernortham40@gmail.com>
2026-09-06 11:27:41 -0400

    Add Feature Importance to Tableau output

--- src/tableau_output.py
+++ src/tableau_output.py
@@ -10,6 +10,7 @@ from sqlalchemy import create_engine
 from sklearn.model_selection import train_test_split
 import numpy as np
 
+# load data
 engine = create_engine("sqlite:///../data/processed/obesity_levels.db")
 
 obesity_levels_sql = pd.read_sql("obesity_levels", con=engine)
@@ -17,9 +18,48 @@ obesity_levels_sql = pd.read_sql("obesity_levels", con=engine)
 obesity_levels_sql.columns = [
     str(col) for col in obesity_levels_sql.columns
 ]
-
+# load model
 model = joblib.load(
     "../models/obesity_levels_rf_model.pkl"
+)
+
+# inspect feature names and importance
+preprocessor = model.named_steps["preprocessor"]
+classifier = model.named_steps["classifier"]
+
+feature_names = preprocessor.get_feature_names_out()
+feature_importances = classifier.feature_importances_
+
+print(feature_names)
+print(len(feature_names))
+print(feature_importances)
+print(len(feature_importances))
+
+feature_importance_df = pd.DataFrame({
+    "Feature": feature_names,
+    "Importance": feature_importances
+})
+
+# clean prefixes
+feature_importance_df["Feature"] = (
+    feature_importance_df["Feature"]
+    .str.replace("num__", "", regex=False)
+    .str.replace("cat__", "", regex=False)
+)
+
+# sort highest to lowest importance
+feature_importance_df = feature_importance_df.sort_values(
+    by="Importance",
+    ascending=False
+)
+
+# inspect
+print(feature_importance_df)
+
+# export to csv for tableau
+feature_importance_df.to_csv(
+    "../data/processed/obesity_feature_importance.csv",
+    index=False
 )
 
 # separate variables from target
